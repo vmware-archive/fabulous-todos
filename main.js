9abd88b0015ecaac83b3d6a089251c4f51d2375a
Brian Cavalier <brian@hovercraftstudios.com>
2015-02-02 13:45:44 -0500

    Switch to localstorage

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 var fab = require('fabulous');
-var rest = require('fabulous/rest');
 var Document = require('fabulous/Document');
+var LocalStorage = require('fabulous/data/LocalStorage');
 var History = require('fabulous/data/History');
 
 var TodosController = require('./TodosController');
 
-exports.main = fab.run(document.body, todosApp);
+exports.main = fab.runAt(todosApp, document.body);
 
 function todosApp(node, context) {
-	var remote = rest.at('/todos');
-
 	context.controller = new TodosController([]);
 	context.history = new History();
 
 	Document.sync([
-		Document.fromPatchRemote(function(patch) {
-			return remote.patch({ entity: patch });
-		}, remote.get()),
+		new LocalStorage('todos'),
 		Document.fromProperty('todos', context.controller),
 		context.history
 	]);
